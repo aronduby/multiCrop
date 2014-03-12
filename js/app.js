378dafbb06d7c07192493afce2da0f39a99c87e2
aronduby <aron.duby@gmail.com>
2014-03-12 18:03:07 -0400

    some generic tweaks to go back to standard jqueryui, get it working with transparent pngs, sweet layout using flex

--- js/app.js
+++ js/app.js
@@ -33,23 +33,55 @@ function MultiCrop(org_img, opts){
 
 	// setup out html
 	var _mc_parent_el = $('<div class="multi-crop"></div>'),
-		_img_holder = $('<div class="img-holder"></div>').appendTo(_mc_parent_el).append(_img),
+		_img_holder = $('<div class="img-holder"></div>').appendTo(_mc_parent_el),
 		_bounds_data_holder = $('<div class="bounds-data"></div>').appendTo(_mc_parent_el),
 		_bounds_data_ul = $('<ul></ul>').appendTo(_bounds_data_holder),
 		_colors = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"],
 		_colors_i = 10;
 
+	_img_holder.append(
+		$('<div></div>')
+			.addClass('padder')
+			.append(_img)
+	);
+
 	org_img.hide();
 	// replaceWith(_mc_parent_el);
 	_mc_parent_el.insertAfter(org_img);
 
+	// figure out what type of image we're going to be saving this as
+	var img_type = null;
+	if(_img.attr('src').substr(0,5)=='data:'){
+		var re = /^data:(image\/\w+)\;/i;
+		var matched = re.exec(_img.attr('src'));
+		if(matched !== null)
+			img_type = matched[1];
+		
+	} else {
+		var ext = _img.attr('src').split('.').pop().toLowerCase();
+		switch(ext){
+			case 'jpg':
+			case 'jpeg':
+				img_type = 'image/jpeg';
+				break;
+			default:
+				img_type = 'image/png';
+		}
+	}
+	if(img_type == 'image/png'){
+		_mc_parent_el.addClass('png');
+	}
+
 	var _obj = {
 		el: _mc_parent_el,
 		img: _img,
+		img_type: img_type,
 		org_width: 0,
 		org_height: 0,
 		org_img: org_img,
 		img_holder: _img_holder,
+		img_loaded: $.Deferred(),
+		bounds_data_holder: _bounds_data_holder,
 		bounds_data_ul: _bounds_data_ul,
 		bounds_data: opts.bounds,
 		bounds: [],
@@ -72,9 +104,14 @@ function MultiCrop(org_img, opts){
 			this.img_holder.removeClass('dim');
 		},
 		save: function(){
+			var data = {
+				src: this.img.attr('src'),
+				bounds: {}
+			};
 			$.each(this.bounds, function(){
-				this.save();
+				data.bounds[this.name] = this.save();
 			});
+			return data;
 		}
 	};
 
@@ -116,11 +153,11 @@ function MultiCrop(org_img, opts){
 	});
 	$(window).on({
 		'window.resize.start.MultiCrop': function(){
-			_obj.img_holder.css('maxWidth', '');
+			//_obj.img_holder.css('maxWidth', '');
 			_obj.dim();
 		},
 		'window.resize.end.MultiCrop': function(){
-			_obj.img_holder.css('maxWidth', _obj.img.width());
+			_obj.bounds_data_holder.css('maxHeight', _obj.img.height());
 			_obj.img.attr({
 				'data-start-width': _obj.img.width(),
 				'data-start-height': _obj.img.height()
@@ -131,19 +168,20 @@ function MultiCrop(org_img, opts){
 	
 	org_img.on('load', function(){
 		_obj.img.attr({
-			'data-start-width': this.width,
-			'data-start-height': this.height,
+			'data-start-width': _obj.img.width(),
+			'data-start-height': _obj.img.height(),
 			'data-org-width': this.width,
 			'data-org-height': this.height
 		});
-		$(window).triggerHandler('window.resize.end.MultiCrop');
+		_obj.bounds_data_holder.css('maxHeight', _obj.img.height());
+		_obj.img_loaded.resolve();
 	});
 
 	return _obj;
 }
 
 function Bound(d, color, _parent, bg_src){
-	var bound_el = $('<div class="bound"></div>').css('backgroundImage', 'url('+bg_src+')').attr('id', 'bound'+Bound.count),
+	var bound_el = $('<div class="bound"></div>').attr('id', 'bound'+Bound.count).append( $('<div class="bg"></div>').css('backgroundImage', 'url('+bg_src+')') ),
 		data_el = $('<li></li>'),
 		canvas = $('<canvas></canvas>');
 
@@ -151,15 +189,15 @@ function Bound(d, color, _parent, bg_src){
 		type: 'exact',
 		finalWidth: 100,
 		finalHeight: 100,
-		width: 0,
-		height: 0,
+		width: 100,
+		height: 100,
 		top: 0,
 		left: 0,
 		borderColor: color,
 		color: color,
-		containment: 'parent',
 		handles: 'all',
-		aspectRatio: 0
+		aspectRatio: 0,
+		maximizeOnStart: true
 	};
 	$.extend(opts, d);
 
@@ -178,18 +216,20 @@ function Bound(d, color, _parent, bg_src){
 		var top = -(el.position().top),
 			left = -(el.position().left);
 		
-		el.css('backgroundPosition', left+'px '+top+'px');
+		el.find('.bg').css('backgroundPosition', left+'px '+top+'px');
 	}
 
 	var _updateThumb = function(el){
-		
-
 		var ctx = _obj.canvas[0].getContext('2d'),
 			p = el.position(),
 			ratio = _parent.img.width() / _parent.img.attr('data-org-width'),
 			bW = parseInt(_obj.bound_el.css('borderLeftWidth'),10) + parseInt(_obj.bound_el.css('borderRightWidth'), 10),
 			bH = parseInt(_obj.bound_el.css('borderTopWidth'),10) + parseInt(_obj.bound_el.css('borderBottomWidth'), 10),
-			dWidth, dHeight
+			dWidth, dHeight;
+
+		if(_parent.img_type == 'image/png'){
+			ctx.clearRect(0,0,_obj.canvas.attr('width'),_obj.canvas.attr('height'));
+		}
 
 		// update the canvas size based on the size of the bounds
 		// change the finalWidth/height to the new size
@@ -228,6 +268,7 @@ function Bound(d, color, _parent, bg_src){
 
 
 	var _obj = {
+		name: opts.name,
 		bound_el: bound_el,
 		data_el: data_el,
 		canvas: canvas,
@@ -252,12 +293,23 @@ function Bound(d, color, _parent, bg_src){
 			_updateThumb(this.bound_el);
 			this.calculateArea();
 		},
+		getBounds: function(){
+			return {
+				top: this.bound_el.position().top,
+				left: this.bound_el.position().left,
+				width: this.bound_el.width(),
+				height: this.bound_el.height()
+			}
+		},
 		toDataURL: function(){ return this.toDataUrl(); },
 		toDataUrl: function(){
-			return this.canvas[0].toDataURL();
+			return this.canvas[0].toDataURL(_parent.img_type, Bound.jpgQuality);
 		},
 		save: function(){
-			this.data_el.find('textarea').val(this.toDataUrl());
+			return {
+				bounds: this.getBounds(),
+				data: this.toDataUrl()
+			};
 		},
 		bringToFront: function(){
 			triggerParentEvent('restack');
@@ -303,13 +355,6 @@ function Bound(d, color, _parent, bg_src){
 				eR = this.opts.aspectRatio!==null ? this.opts.aspectRatio : 0,
 				dims = [];
 
-			// issues when it's first called when the bounds w/h hasn't been set
-			if(eW == 0 || eH == 0){
-				eW = eW ? eW : 10;
-				eH = eR ? Math.round(eW / eR) : 10;
-			}
-
-
 			// rs > ri ? (wi * hs/hi, hs) : (ws, hi * ws/wi)
 			// ratioScreen > ratioImage ? (widthImage * heightScreen/heightImage, heightScreen) : (widthScreen, heightImage * widthScreen/widthImage)
 			if(eR !== 0){
@@ -354,51 +399,56 @@ function Bound(d, color, _parent, bg_src){
 	});
 
 	data_el
-		.append($('<p class="name">'+(d.name ? d.name : 'untitled')+'</p>'))
-		.append($('<p class="btns"></p>').append(btns))
-		.append(canvas)
-		.append('<p class="note">'+(opts.type=='fit' ? 'fit within: ':"final size: ")+opts.finalWidth+'x'+opts.finalHeight+'</p>')
-		.append('<textarea name="'+(opts.name)+'"></textarea>')
-		.css('borderColor', color);
+		.append($('<header>'+d.name+'</header>').css('backgroundColor',color))
+		.append(
+			$('<div></div>')
+				.append($('<p class="btns"></p>').append(btns))
+				.append(canvas)
+				.append('<p class="note">'+(opts.type=='fit' ? 'fit within: ':"final size: ")+opts.finalWidth+'x'+opts.finalHeight+'</p>')
+		);
 	bound_el.append('<label>'+(d.name ? d.name : 'untitled')+'</label>');
 
 	bound_el.appendTo(_parent.img_holder);
 	data_el.appendTo(_parent.bounds_data_ul);
 
+	var _onResize = function(){
+		var sW = _parent.img.attr('data-start-width'),
+			eW = _parent.img.width(),
+			eH = _parent.img.height(),
+			delta = eW/sW,
+			nT = Math.round(_obj.bound_el.position().top * delta),
+			nL = Math.round(_obj.bound_el.position().left * delta),
+			nW = Math.round(_obj.bound_el.width() * delta),
+			nH = Math.round(_obj.bound_el.height() * delta);
+		
+		_obj.bound_el.css({
+			top: nT,
+			left: nL,
+			width: nW,
+			height: nH,
+		});
+		_obj.bound_el.find('.bg').css('backgroundSize', eW+'px '+eH+'px');
+
+		_obj.update();
+	};
 
 	$(window).on({
 		'window.resize.start.MultiCrop': function(){
 			
 		},
 		'window.resize.end.MultiCrop': function(){
-			var sW = _parent.img.attr('data-start-width'),
-				eW = _parent.img.width(),
-				eH = _parent.img.height(),
-				bW = parseInt(_obj.bound_el.css('borderLeftWidth'),10) + parseInt(_obj.bound_el.css('borderRightWidth'), 10),
-				bH = parseInt(_obj.bound_el.css('borderTopWidth'),10) + parseInt(_obj.bound_el.css('borderBottomWidth'), 10),
-				delta = eW/sW
-				nT = Math.round(_obj.bound_el.position().top * delta),
-				nL = Math.round(_obj.bound_el.position().left * delta),
-				nW = Math.round(_obj.bound_el.width() * delta),
-				nH = Math.round(_obj.bound_el.height() * delta);
-			
-			_obj.bound_el.css({
-				top: nT,
-				left: nL,
-				width: nW,
-				height: nH,
-				backgroundSize: eW+'px '+eH+'px',
-			});
-
-			_obj.update();
-
+			_onResize();
 		}
 	});
 
-	_parent.img.on('load', function(){
-		if(_obj.opts.width == 0 || _obj.opts.height == 0)
-			_obj.maximize();
+	data_el.on('click', 'header', function(){
+		$(this).parent('li').toggleClass('open');
+	});
 
+	_parent.img_loaded.then(function(){
+		_onResize();
+		if(_obj.opts.maximizeOnStart)
+			_obj.maximize();
 		_obj.update();
 	});
 
@@ -429,6 +479,10 @@ function Bound(d, color, _parent, bg_src){
 	});
 
 	//css
+	if(opts.aspectRatio){
+		if(opts.width / opts.height != opts.aspectRatio)
+			opts.height = opts.width / opts.aspectRatio;
+	}
 	bound_el.css({
 		height: opts.height,
 		width: opts.width,
@@ -440,10 +494,7 @@ function Bound(d, color, _parent, bg_src){
 
 	// resizable
 	bound_el.resizable({
-		containment: {
-			container: opts.containment,
-			resizeInPadding: true
-		},
+		containment: '.multi-crop .img-holder',
 		handles: opts.handles,
 		aspectRatio: opts.aspectRatio,
 		minWidth: opts.minWidth,
@@ -468,11 +519,7 @@ function Bound(d, color, _parent, bg_src){
 
 	// draggable
 	bound_el.draggable({
-		containment: {
-			container: opts.containment,
-			dragInPadding: true
-		},
-		
+		containment: '.multi-crop .img-holder',		
 		drag: function(){
 			_obj.update();
 		},
@@ -489,9 +536,10 @@ function Bound(d, color, _parent, bg_src){
 		}
 	});
 
-	_obj.update();
+	// _obj.update();
 	Bound.count++;
 	
 	return _obj;
 }
 Bound.count = 0;
+Bound.jpgQuality = .75;
